ea77f4d961d0395be6ced903afe9f248aaa4e43d
iiivvveee <87736192+iiivvveee@users.noreply.github.com>
2021-07-21 08:51:27 +0900

    Create boj_2935.py

---
+++ done/boj_2935.py
@@ -0,0 +1,13 @@
+"""
+  Written by Younsung Lee on Jul. 21. 2021.
+  BOJ 2935 "BUKA"
+"""
+
+num1 = int(input())
+op = input()
+num2 = int(input())
+
+if op == '*':
+	print(num1 * num2)
+elif op == '+':
+	print(num1 + num2)
