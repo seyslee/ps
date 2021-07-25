788c3447decb04ca697ec896b24fbca032ce46c9
iiivvveee <87736192+iiivvveee@users.noreply.github.com>
2021-07-25 13:27:34 +0900

    Create boj_10214.py

---
+++ done/boj_10214.py
@@ -0,0 +1,20 @@
+"""
+  Written by Younsung Lee on Jul. 25. 2021.
+  BOJ 10214 "Baseball"
+"""
+
+case = int(input())
+score_y, score_k = 0, 0
+
+for _ in range(case):
+    for _ in range(9):
+        y, k = map(int, input().split())
+        score_y += y
+        score_k += k
+
+    if score_y > score_k:
+        print("Yonsei")
+    elif score_y < score_k:
+        print("Korea")
+    else:
+        print("Draw")
