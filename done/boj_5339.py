e452bfdabb79ece4fea1e286dd29de7129a506c3
iiivvveee <87736192+iiivvveee@users.noreply.github.com>
2021-07-27 14:59:18 +0900

    Create boj_5339.py

---
+++ done/boj_5339.py
@@ -0,0 +1,15 @@
+"""
+  Written by Younsung Lee on Jul. 27. 2021.
+  BOJ 5339 "Call For Help"
+"""
+
+print("     /~\\")
+print("    ( oo|")
+print("    _\\=/_")
+print("   /  _  \\")
+print("  //|/.\\|\\\\")
+print(" ||  \ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
