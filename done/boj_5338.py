1d67d51c809344bb7e07dfd6dddcabb108ee9d6e
iiivvveee <87736192+iiivvveee@users.noreply.github.com>
2021-07-27 14:51:06 +0900

    Create boj_5338.py

---
+++ done/boj_5338.py
@@ -0,0 +1,10 @@
+"""
+  Written by Younsung Lee on Jul. 27. 2021.
+  BOJ 5338 "Microsoft Logo"
+"""
+
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
