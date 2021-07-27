2aa18c9ac789013fa020380e3700b7805892fd36
iiivvveee <87736192+iiivvveee@users.noreply.github.com>
2021-07-27 16:31:04 +0900

    Create boj_9654.py

---
+++ done/boj_9654.py
@@ -0,0 +1,13 @@
+"""
+  Written by Younsung Lee on Jul. 27. 2021.
+  BOJ 9654 "Naboo Fleet Data"
+"""
+
+print(
+"""SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+N2 Bomber      Heavy Fighter  Limited    21        
+J-Type 327     Light Combat   Unlimited  1         
+NX Cruiser     Medium Fighter Limited    18        
+N1 Starfighter Medium Fighter Unlimited  25        
+Royal Cruiser  Light Combat   Limited    4         """
+)
