bde5f26c38ac49c090e54f43211a291c3957fe87
iiivvveee <87736192+iiivvveee@users.noreply.github.com>
2021-08-12 11:01:59 +0900

    Create boj_21300.py

---
+++ done/boj_21300.py
@@ -0,0 +1,8 @@
+"""
+  Written by Younsung Lee on Aug. 12. 2021.
+  BOJ 21300 "Bottle Return"
+"""
+
+be, ma, wi, ca, se, wa = map(int, input().split())
+total = (be + ma + wi + ca + se + wa) * 5
+print(total)
