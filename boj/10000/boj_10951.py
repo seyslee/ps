52293f97ff1bdd2755e84f7a1880626a2f8c8692
Younsung Lee <87736192+seyslee@users.noreply.github.com>
2021-09-14 13:20:48 +0900

    Create boj_10951.py

---
+++ boj/10000/boj_10951.py
@@ -0,0 +1,14 @@
+"""
+    Author  : Younsung Lee
+    Date    : 2021-09-14
+    Site    : BOJ
+    Pr Num  : 10951
+    Pr Info : A+B - 4
+"""
+
+while True:
+	try:
+		a, b = map(int, input().split())
+		print(a + b)
+	except:
+		break
