e43e95ac8c7426b072d9521eb58b42734c38959d
Younsung Lee <87736192+seyslee@users.noreply.github.com>
2021-09-14 13:30:44 +0900

    Create boj_15740.py

---
+++ boj/15000/boj_15740.py
@@ -0,0 +1,14 @@
+"""
+    Author  : Younsung Lee
+    Date    : 2021-09-14
+    Site    : BOJ
+    Pr Num  : 15740
+    Pr Info : A+B - 9
+"""
+
+while True:
+	try:
+		a, b = map(int, input().split())
+		print(a + b)
+	except:
+		break
