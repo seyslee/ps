7cc29715ba3f5824d1fb95826329085993671c7d
Younsung Lee <87736192+seyslee@users.noreply.github.com>
2021-09-14 13:22:23 +0900

    Create boj_10953.py

---
+++ boj/10000/boj_10953.py
@@ -0,0 +1,13 @@
+"""
+    Author  : Younsung Lee
+    Date    : 2021-09-14
+    Site    : BOJ
+    Pr Num  : 10953
+    Pr Info : A+B - 6
+"""
+
+c = int(input())
+
+for _ in range(c):
+	a, b = map(int, input().split(','))
+	print(a + b)
