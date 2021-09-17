e16619f19eb5dad699b7a58c8bf6aa172339d441
Younsung Lee <87736192+seyslee@users.noreply.github.com>
2021-09-17 09:02:08 +0900

    Create boj_10818.py

---
+++ boj/10000/boj_10818.py
@@ -0,0 +1,11 @@
+"""
+    Author  : Younsung Lee
+    Date    : 2021-09-17
+    Site    : BOJ
+    Pr Num  : 10818
+    Pr Info : 최소, 최대
+"""
+
+c = int(input())
+nums = list(map(int, input().split()))
+print(min(nums), max(nums))
