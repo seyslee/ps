b1fe81e6fd6989e226224d70923881c58699f218
Younsung Lee <87736192+seyslee@users.noreply.github.com>
2021-09-27 08:58:01 +0900

    Create boj_11365.py

---
+++ boj/11000/boj_11365.py
@@ -0,0 +1,13 @@
+"""
+    Author  : Younsung Lee
+    Date    : 2021-09-27
+    Site    : BOJ
+    Pr Num  : 11365
+    Pr Info : !밀비 급일
+"""
+
+while True:
+	sentence = input()
+	if sentence == 'END':
+		break
+	print(sentence[::-1])
