380ff4409100298c71fb7b425affadf3773d5b99
Younsung Lee <87736192+seyslee@users.noreply.github.com>
2021-10-14 16:49:53 +0900

    Create boj_10821.py

---
+++ boj/10000/boj_10821.py
@@ -0,0 +1,10 @@
+"""
+    Author  : Younsung Lee
+    Date    : 2021-10-14
+    Site    : BOJ
+    Pr Num  : 10821
+    Pr Info : 정수의 개수
+"""
+
+numbers = list(map(int, input().split(',')))
+print(len(numbers))
